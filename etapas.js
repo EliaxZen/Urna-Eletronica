89a612a9f8f3e65e97391ea11b0b83e851b2821b
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-09-06 12:22:05 -0300

    upload dos arquivos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '38111',
+              nome: 'Gato Maromba',
+              partido: 'PGM',
+              fotos:[
+                  {url:'38111.jpg', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '77222',
+              nome: 'Sorriso',
+              partido: 'PSPT',
+              fotos:[
+                  {url:'77222.jpg', legenda: 'Vereador'}
+              ]
+          },
+      ]
+  },
+  {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '99',
+              nome: 'Capibara',
+              partido: 'ABC',
+              vice: 'Pingo😪',
+              fotos:[
+                  {url:'99.jpg', legenda: 'Prefeito'},
+                  {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+          {
+              numero: '84',
+              nome: 'Janilson',
+              partido: 'QWERTY',
+              vice: 'Caio',
+              fotos:[
+                  {url:'84.jpg', legenda: 'Prefeito'},
+                  {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+      ]
+  }
+];
